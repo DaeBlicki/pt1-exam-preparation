5d808a43d9de77ed3b3197d79d86ebf05e0a4afe
davidbl@student.ethz.ch <david.blickenstorfer@gmail.com>
2024-01-29 15:08:04 +0100

    initialize lotka volterra

--- pyPenna/solutions/pypenna/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Programming Techniques for Scientific Simulations, ETH Zurich
-
-from .population import *
-from .animal import *
-from .genome import *
-from .fishing import *
